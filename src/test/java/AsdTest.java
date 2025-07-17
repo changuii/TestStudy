ff2e69ec90dab7ff41cea69024667e447a64f81a
changuii <rhljh7410@gmail.com>
2025-07-17 12:54:08 +0900

    test

---
+++ src/test/java/AsdTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AsdTest {
+
+    @DisplayName("asd")
+    @Test
+    void asd() {
+        // given
+        System.out.println("asd");
+        // when
+
+        // then
+    }
+}
